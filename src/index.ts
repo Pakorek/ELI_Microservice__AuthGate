f4b82f84ed1555527d4574a7c9e45dd98bf0771d
pakorek <brice.darmenia@sfr.fr>
2021-04-07 14:36:07 +0200

    sort files

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
 import 'reflect-metadata';
 import { User } from './entity/User';
-import { Quiz } from './entity/Quiz';
-import { Answer } from './entity/Answer';
-import { Upload } from './entity/Upload';
-import { Course } from './entity/Course';
-import { Question } from './entity/Question';
-import { Promotion } from './entity/Promotion';
-import { Tag } from './entity/Tag';
-import { Evaluation } from './entity/Evaluation';
 import { ContactInformation } from './entity/ContactInformation';
-import { CourseResolver } from './resolvers/Course';
 import { UserResolver } from './resolvers/User';
-import { QuizResolver } from './resolvers/Quiz';
 import { passwordAuthChecker } from './utils/auth-checker';
 import { createConnection } from 'typeorm';
 import { buildSchema } from 'type-graphql';
@@ -20,9 +10,6 @@ import Express from 'express';
 import dotenv from 'dotenv';
 import cors = require('cors');
 import cookieParser = require('cookie-parser');
-import { QuestionResolver } from './resolvers/Question';
-import { AnswerResolver } from './resolvers/Answer';
-import { TagResolver } from './resolvers/Tag';
 
 dotenv.config();
 
@@ -36,15 +23,7 @@ const startServer = async () => {
     database: 'ELI',
     entities: [
       User,
-      Course,
-      Promotion,
-      Upload,
-      Evaluation,
       ContactInformation,
-      Quiz,
-      Question,
-      Answer,
-      Tag,
     ],
     synchronize: true,
     migrations: ['migration/*.ts'],
@@ -56,11 +35,6 @@ const startServer = async () => {
   const schema = await buildSchema({
     resolvers: [
       UserResolver,
-      CourseResolver,
-      QuizResolver,
-      QuestionResolver,
-      AnswerResolver,
-      TagResolver,
     ],
     authChecker: passwordAuthChecker,
     nullableByDefault: true,
